2b1f377fa59c525dabe7ec212e9e713049eed837
David Fontenot <dave@hackmatch.com>
2025-01-21 08:48:26 -0800

    Initial Commit

---
+++ foundermeditations/foundermeditationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  foundermeditationsApp.swift
+//  foundermeditations
+//
+//  Created by hellyeah on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct foundermeditationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foundermeditationsTests/foundermeditationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  foundermeditationsTests.swift
+//  foundermeditationsTests
+//
+//  Created by hellyeah on 1/21/25.
+//
+
+import Testing
+@testable import foundermeditations
+
+struct foundermeditationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

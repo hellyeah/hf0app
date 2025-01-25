cdbed5a1e57fbde816ebfffd222c3a78828bca2e
David Fontenot <dave@hackmatch.com>
2025-01-25 12:06:10 -0800

    added hf0 spinny sphere gif and got it working

--- foundermeditations/ContentView.swift
+++ foundermeditations/ContentView.swift
@@ -6,9 +6,32 @@
 //
 
 import SwiftUI
+import SwiftyGif
 
 struct ContentView: View {
+
+    
+    struct AnimatedGifView: UIViewRepresentable {
+        @Binding var url: URL
+
+        func makeUIView(context: Context) -> UIImageView {
+            let imageView = UIImageView(gifURL: self.url)
+            imageView.contentMode = .scaleAspectFit
+            return imageView
+        }
+
+        func updateUIView(_ uiView: UIImageView, context: Context) {
+            uiView.setGifFromURL(self.url)
+        }
+    }
+    
+
+    
     var body: some View {
+//        Circle()
+//            .fill(.blue)
+//            .padding()
+        GIFView(gifName: "spinny.gif")
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
@@ -16,8 +39,28 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        
+        
+
+        
     }
 }
+
+struct GIFView: UIViewRepresentable {
+    let gifName: String
+    
+    func makeUIView(context: Context) -> UIImageView {
+        guard let gif = try? UIImage(gifName: gifName) else {
+            return UIImageView()
+        }
+        let imageView = UIImageView(gifImage: gif, loopCount: 12)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }
+    
+    func updateUIView(_ uiView: UIImageView, context: Context) {}
+}
+
 
 #Preview {
     ContentView()
